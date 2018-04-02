7f0f845965381deae0f7b40d59166254ac1ed26c
roy <roygiladix@gmail.com>
2018-04-02 17:41:28 +0300

    configure services

--- server/config/service-config.js
+++ server/config/service-config.js
@@ -1,43 +1,12 @@
-ServiceConfiguration.configurations.upsert(
-  { service: 'google' },
-  {
-    $set: {
-      loginStyle: "popup",
-      clientId: "79617251768-7ab1bdn50fr9mm5jklpcjffu3r9fndgb.apps.googleusercontent.com",
-      secret: "iECBk1_SVhAuR0-dxgsgSMoo"
-    }
-  }
-);
+import { Meteor } from 'meteor/meteor'
 
-ServiceConfiguration.configurations.upsert(
-  { service: 'facebook' },
-  {
-    $set: {
-      loginStyle: 'popup',
-      appId: '196447867795092',
-      secret: '661783b9165324b5f8be3c28bf2383b4'
-    }
-  }
-)
-
-ServiceConfiguration.configurations.upsert(
-  { service: 'github' },
-  {
-    $set: {
-      loginStyle: 'popup',
-      clientId: 'da0e6be9fe5fc3f13282',
-      secret: '336bd622a9dbcd396e84ecdf1a6e4c2908ead7f7'
-    }
-  }
-)
-
-ServiceConfiguration.configurations.upsert(
-  { service: 'twitter' },
-  {
-    $set: {
-      loginStyle: 'popup',
-      consumerKey: 'ZTZs0tLPhgyULKzTSye5hgjHp',
-      secret: 'aikdJl9fgcSmVMEjctt5Xi0VVjycAAen0kdYbLyNpNn0RPyazS'
-    }
-  }
-)
+const { services } = Meteor.settings
+Object.keys(services)
+  .forEach(service => {
+    ServiceConfiguration.configurations.upsert(
+      { service },
+      {
+        $set: services[service]
+      }
+  )
+})
